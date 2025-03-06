839a6ea51e09e99bcf9e3ab68daeed64f50a08a4
clys-man <clysmanalves@gmail.com>
2025-03-06 14:14:57 -0300

    feat: remove phpactor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -228,9 +228,8 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'phpactor',
+      'intelephense',
       'php-debug-adapter',
-      'phpstan',
       'pint',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
