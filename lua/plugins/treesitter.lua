a37f7fa387606b41db0f6ddf6f1fdf740abcf450
clys-man <clysmanalves@gmail.com>
2025-08-22 19:50:50 -0300

    feat: add bend language

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,60 @@
-return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'go',
-      'gitignore',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-      'php',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local configs = require 'nvim-treesitter.configs'
+
+      configs.setup {
+        ensure_installed = {
+          'bend',
+          'lua',
+          'python',
+          'javascript',
+          'typescript',
+          'vimdoc',
+          'vim',
+          'regex',
+          'sql',
+          'dockerfile',
+          'toml',
+          'json',
+          'java',
+          'go',
+          'gitignore',
+          'yaml',
+          'make',
+          'cmake',
+          'markdown',
+          'markdown_inline',
+          'bash',
+          'tsx',
+          'css',
+          'html',
+          'php',
+          'rust',
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.bend = {
+        install_info = {
+          url = 'https://github.com/HigherOrderCO/tree-sitter-bend',
+          files = { 'src/parser.c', 'src/scanner.c' },
+          branch = 'main',
+        },
+      }
+
+      vim.filetype.add {
+        extension = {
+          bend = 'bend',
+        },
+      }
+
+      vim.treesitter.language.register('bend', { 'bend' })
+    end,
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
